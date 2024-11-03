c0e694a88ca33a8a4b1592701a5fea8199eecf4c
kellen xavier <kellenxaviier@gmail.com>
2024-11-03 15:03:54 -0300

    feat: foi modificado o frontend pag home + add links no menu

--- app/components/projetos.tsx
+++ app/components/projetos.tsx
@@ -15,21 +15,21 @@ interface ProjectsProps {
 
 export const projectsData = [
   {
-    image: '#', 
+    image: '#',
     name: 'Studio Ghibli',
-    description: 'Desenvolvimento de um Layout simples, para praticas básicas de frontend, estruturas de página.',
-    technologies: ['CSS', 'HTML', 'Javascript'],
+    description: 'Desenvolvimento de um Layout simples, para práticas básicas de frontend e estrutura de página.',
+    technologies: ['CSS', 'HTML', 'JavaScript'],
     projectLink: 'https://project-studio-ghibli.netlify.app/',
-    repoLink: 'https://github.com/kellen-xavier/page-studio-ghibli', 
+    repoLink: 'https://github.com/kellen-xavier/page-studio-ghibli',
   },
 ];
 
 export function Projects({ projects }: Readonly<ProjectsProps>) {
   return (
     <div className={styles.projectsContainer}>
-      {projects.map((project) => (
+      {projects.slice(0, 4).map((project) => (
         <div key={project.name} className={styles.projectCard}>
-          <image className={styles.projectImage}>Like</image>
+          <img src={project.image} alt={`Imagem do projeto ${project.name}`} className={styles.projectImage} />
           <h3 className={styles.projectName}>{project.name}</h3>
           <p className={styles.projectDescription}>{project.description}</p>
           <div className={styles.technologies}>
@@ -41,7 +41,7 @@ export function Projects({ projects }: Readonly<ProjectsProps>) {
             <a href={project.projectLink} target="_blank" rel="noopener noreferrer" className={styles.projectButton}>
               Acessar o projeto
             </a>
-            <a href={project.repoLink} target="_blank" rel="noopener noreferrer" className={styles.projectButton}>
+            <a href={project.repoLink} target="_blank" rel="noopener noreferrer" className={styles.projectButtonOutline}>
               Acessar repositório
             </a>
           </div>
